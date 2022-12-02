7f4ec88602f88520d4bb335d080518569e4c575d
Thien Nguyen <nqthien041292@gmail.com>
2022-12-02 15:01:51 +0700

    Update source code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="pawankolhe/projectml:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run projectml --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward projectml 8000:80
