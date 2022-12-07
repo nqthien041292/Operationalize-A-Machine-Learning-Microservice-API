e990c40e20fd30c61f1ab124936e7df805f5300d
Thien Nguyen <nqthien041292@gmail.com>
2022-12-07 14:31:04 +0700

    Update Kubenetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="pawankolhe/projectml:latest"
+dockerpath="nqthien041292/operationalize-a-machine-learning-microservice-api:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run projectml --image=$dockerpath --port=80
+kubectl run learning-microservice-api --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward projectml 8000:80
+kubectl port-forward learning-microservice-api 8000:80
